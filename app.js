95122244ac7af9cfae74e75b7decdc8945bcddda
Jodi Detch <jodidetch@gmail.com>
2014-08-07 10:50:32 -0400

    App has app.js file with IIFE & some routes defined

---
+++ app.js
@@ -0,0 +1,20 @@
+// IIFE for the customers app
+(function customersAppIIFE(){
+
+// Defining the App module that will contain
+// all our controllers, views, services and factories
+
+// Our app depends on the ngRoute library
+  var app = angular.module('customersApp', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+      .when('/',
+        {
+          controller: 'customersController',
+          templateUrl: 'app/views/customers.html'
+        }
+      )
+      .otherwise({redirectTo: '/'});
+  });
+})();
